25573076fc3d68f9349b7bf52344f894e46f0bd1
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-12-12 22:58:58 +0300

    added state for all questions and when the data is loading

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,33 @@ import QuestionList from "./QuestionList";
 function App() {
   const [page, setPage] = useState("List");
 
+  // State for all questions
+  const [allQuestions, setAllQuestions] = useState([])
+
+  // State for when data is loading
+  const [isLoading, setIsLoading] = useState(true)
 
   // useState for fetching data from the API
   useEffect(() => (
     fetch("http://localhost:4000/questions")
       .then(response => response.json())
       .then(data => {
-        console.log(data)
+        setAllQuestions(data)
+        setIsLoading(false)
       })
+      .catch(error => console.error("An error occured when fetching data!", error))
   ), [])
+
+  if (isLoading) {
+    return (
+      <h2>Loading Questions...</h2>
+    )
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList quizes={allQuestions} />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-function QuestionList() {
+function QuestionList({ quizes }) {
+  console.log(quizes)
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {/* display QuestionItem components here after fetching */}
+
+      </ul>
     </section>
   );
 }
