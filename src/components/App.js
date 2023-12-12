cbb3188ea7c1adfba17e93b9455ab617481d2a39
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-12-12 15:40:50 +0300

    useState for fetching data from the API

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+
+
+  // useState for fetching data from the API
+  useEffect(() => (
+    fetch("http://localhost:4000/questions")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+  ), [])
 
   return (
     <main>
