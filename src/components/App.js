fe8eef0120babdbcba2e77ee923950b76f09c572
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-12-13 09:05:27 +0300

    moved the questions state to the questionList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
@@ -6,34 +6,10 @@ import QuestionList from "./QuestionList";
 function App() {
   const [page, setPage] = useState("List");
 
-  // State for all questions
-  const [allQuestions, setAllQuestions] = useState([])
-
-  // State for when data is loading
-  const [isLoading, setIsLoading] = useState(true)
-
-  // useState for fetching data from the API
-  useEffect(() => (
-    fetch("http://localhost:4000/questions")
-      .then(response => response.json())
-      .then(data => {
-        setAllQuestions(data)
-        setIsLoading(false)
-      })
-      .catch(error => console.error("An error occured when fetching data!", error))
-  ), [])
-
-  // If statement for setting the loading function
-  if (isLoading) {
-    return (
-      <h2>Loading Questions...</h2>
-    )
-  }
-
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList quizes={allQuestions} />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import QuestionItem from "./QuestionItem"
 
-function QuestionList({ quizes }) {
-  console.log(quizes)
+function QuestionList() {
+
+  // State for all questions
+  const [allQuestions, setAllQuestions] = useState([])
+
+  // State for when data is loading
+  const [isLoading, setIsLoading] = useState(true)
+
+  // useState for fetching data from the API
+  useEffect(() => (
+    fetch("http://localhost:4000/questions")
+      .then(response => response.json())
+      .then(data => {
+        setAllQuestions(data)
+        setIsLoading(false)
+      })
+      .catch(error => console.error("An error occured when fetching data!", error))
+  ), [])
+
+  // If statement for setting the loading function
+  if (isLoading) {
+    return (
+      <h2>Loading Questions...</h2>
+    )
+  }
+
 
   return (
     <section>
       <h1>Quiz Questions</h1>
       <ul>
         {/* display QuestionItem components here after fetching */}
-        {quizes.map((question) => {
+        {allQuestions.map((question) => {
           return (
             <QuestionItem key={question.id} question={question} />)
         })}
